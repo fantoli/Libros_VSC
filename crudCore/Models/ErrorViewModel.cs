eb9bdcba5aa70fde8f93cc1f0046b9627203e538
NICEWARE\amartinez <admarmaz@gmail.com>
2021-06-23 08:53:42 +0200

    Agregar archivos de proyecto.

---
+++ crudCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace crudCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
