fc399f05f422708515eb292af356416dfb345e08
MrTolins <ferantoli22@gmail.com>
2021-06-30 09:18:57 +0200

    Eliminar y editar libro

--- crudCore/Controllers/LibrosController.cs
+++ crudCore/Controllers/LibrosController.cs
@@ -32,15 +32,96 @@ namespace crudCore.Controllers
 
         //Http Post Create
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public IActionResult Create(Libro libro)
         {
             if (ModelState.IsValid)
             {
                 _context.Libro.Add(libro);
                 _context.SaveChanges();
+
+                TempData["mensaje"] = "El libro se ha creado correctamente";
+                return RedirectToAction("Index");
             }
 
             return View();
+        }
+
+        //Http Get Edit
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            //obtener el libro
+            var libro = _context.Libro.Find(id);
+
+            if (libro == null)
+            {
+                return NotFound();
+            }
+
+            return View(libro);
+        }
+
+        //Http Post Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Libro libro)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Libro.Update(libro);
+                _context.SaveChanges();
+
+                TempData["mensaje"] = "El libro se ha actualizado correctamente";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        //Http Get Delete
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            //obtener el libro
+            var libro = _context.Libro.Find(id);
+
+            if (libro == null)
+            {
+                return NotFound();
+            }
+
+            return View(libro);
+        }
+
+        //Http Post Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteLibro(int? id)
+        {
+            //obtener el libro por id
+            var libro = _context.Libro.Find(id);
+
+            if (libro == null)
+            {
+                return NotFound();
+            }
+
+         
+            _context.Libro.Remove(libro);
+            _context.SaveChanges();
+
+            TempData["mensaje"] = "El libro se ha eliminado correctamente";
+            return RedirectToAction("Index");
+
         }
     }
 }
